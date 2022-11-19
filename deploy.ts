7acaa85623996175d38c0b9ea65bda08974ab76d
tmpITK <torok.mark.patrik@gmail.com>
2022-11-19 11:43:57 +0100

    convert to typescript

---
+++ deploy.ts
@@ -0,0 +1,42 @@
+/* const ethers = require("ethers")
+const fs = require("fs-extra")
+require("dotenv").config() */
+
+import { ethers } from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL!)
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
+  //const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8");
+  //let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //encryptedJson,
+  //process.env.PRIVATE_KEY_PASSWORD
+  //);
+  // wallet = await wallet.connect(provider);
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  )
+
+  const contractFactor = new ethers.ContractFactory(abi, binary, wallet)
+  console.log("Deploying, please wait...")
+  const contract = await contractFactor.deploy()
+  await contract.deployTransaction.wait(1)
+
+  const currentFavouriteNumber = await contract.retrieve()
+  console.log(`Current fav num: ${currentFavouriteNumber.toString()}`)
+  const txRespone = await contract.store("7")
+  txRespone.wait(1)
+  const newFavouriteNumber = await contract.retrieve()
+  console.log(`New fav num: ${newFavouriteNumber.toString()}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
